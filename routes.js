f72930c1248aa4dd10105470f88dfc94b3b729ef
Christofer Flores <cristofer.flort@gmail.com>
2018-11-06 20:52:13 -0600

    Fix the ui of the Scanner a little bit

--- routes.js
+++ routes.js
@@ -12,7 +12,7 @@ export default createStackNavigator(
   }, 
   {
     navigationOptions: {
-      header: null,
+      header: null
     },
     initialRouteName: 'Scanner'
   }
--- src/pages/Scanner/Scanner.js
+++ src/pages/Scanner/Scanner.js
@@ -6,11 +6,16 @@ export default class App extends React.Component {
   state = {
     hasPermissionsGranted: null,
     type: BarCodeScanner.Constants.Type.back,
+    showInfo: false
   };
 
   async componentDidMount() {
     let { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasPermissionsGranted: (status === 'granted') });
+  }
+
+  onCodeRead = ({ data }) => {
+    this.setState({ showInfo: true })
   }
 
   render() {
@@ -22,20 +27,21 @@ export default class App extends React.Component {
       return <Text>No access to camera</Text>;
     }
     return (
-      <View style={{ flex: 1 }}>
+      <View style={styles.container}>
         <BarCodeScanner
-          onBarCodeScanned={data => alert(JSON.stringify(data))}
+          onBarCodeScanned={this.state.showInfo ? undefined : this.onCodeRead}
           barCodeTypes={[
             BarCodeScanner.Constants.BarCodeType.qr,
           ]}
           type={this.state.type}
-          style={{ ...StyleSheet.absoluteFillObject }}
+          style={StyleSheet.absoluteFill}
         />
-        <TouchableOpacity
+        <View
           style={{
             flex: 1,
             alignSelf: 'flex-end',
             alignItems: 'center',
+            width: '100%'
           }}
           onPress={() => this.setState({ type:
             this.state.type === BarCodeScanner.Constants.Type.back
@@ -43,8 +49,9 @@ export default class App extends React.Component {
               : BarCodeScanner.Constants.Type.back,
           })}
         >
-          <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-        </TouchableOpacity>
+          <Text style={styles.instructions}> Escanea el codigo QR para marcar la asistencia </Text>
+          <View style={styles.selectableArea}></View>
+        </View>
       </View>
     );
   }
@@ -56,5 +63,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 50
   },
+  selectableArea: {
+    borderColor: 'red',
+    borderWidth: 2,
+    height: 200,
+    width: 200,
+    marginTop: '40%'
+  },
+  instructions: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: 'black',
+    paddingVertical: 15,
+    paddingHorizontal: 5,
+    backgroundColor: 'rgba(255,255,255,0.6)'
+  }
 });
